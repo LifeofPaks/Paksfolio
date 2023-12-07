bc3b2c1e283337ed2c6ae5ece3df6b1b74597633
Paks <promise.chukwudi@yahoo.com>
2023-12-07 07:32:02 +0100

    updated

--- src/Helpers/ProjectsData.js
+++ src/Helpers/ProjectsData.js
@@ -1,16 +1,24 @@
 import Port1 from "../assets/images/pakseverything.png";
 import Port2 from "../assets/images/Facebook-clone.png";
-import Port3 from "../assets/images/Product Image.png";
+import Port3 from "../assets/images/Rivero.png";
 import Port4 from "../assets/images/linkedin-clone.png";
 import Port5 from "../assets/images/Paks homes.png";
 import Port6 from "../assets/images/Admin Dashboard.png";
 
 export const ProjectData = [
   {
+    title: "Rivero Trust Bank Plc",
+    image: Port3,
+    url: "https://riverotrustbankplc.netlify.app/",
+    animationDelay: "w-1",
+    tools: "NextJS, Typescript, SCSS",
+    id: 3,
+  },
+  {
     title: "Paks Homes Real Estate",
     image: Port5,
     url: "https://pakshomes.netlify.app/",
-    animationDelay: "w-1",
+    animationDelay: "w-2",
     tools: "REACT, Typescript, Firebase, SCSS",
     id: 5,
   },
@@ -18,7 +26,7 @@ export const ProjectData = [
     title: "Paks Everything Store",
     image: Port1,
     url: "https://pakseverythingstore.netlify.app/",
-    animationDelay: "w-2",
+    animationDelay: "w-3",
     tools: "REACT, SCSS",
     id: 1,
   },
@@ -27,7 +35,7 @@ export const ProjectData = [
     title: "Admin Dashboard",
     image: Port6,
     url: "https://paks-admin-dashboard.netlify.app/",
-    animationDelay: "w-3",
+    animationDelay: "w-4",
     tools: "REACT, SASS, RECHARTS, MUI",
     id: 6,
   },
@@ -36,18 +44,9 @@ export const ProjectData = [
     title: "Facebook Clone",
     image: Port2,
     url: "https://facebook-clone-paks.netlify.app/",
-    animationDelay: "w-4",
+    animationDelay: "w-5",
     tools: "HTML, CSS, JavaScript",
     id: 2,
-  },
-
-  {
-    title: "Product Page",
-    image: Port3,
-    url: "https://product-pagex.netlify.app/",
-    animationDelay: "w-5",
-    tools: "REACT, SCSS",
-    id: 3,
   },
 
   {
