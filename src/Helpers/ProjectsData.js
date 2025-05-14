9c424dd5885251deb16c36fae09b2dd5e487bf41
lifeofpaks <chukwudi.promise@yahoo.com>
2025-05-14 10:24:48 +0100

    updated portfolio links and images

--- src/Helpers/ProjectsData.js
+++ src/Helpers/ProjectsData.js
@@ -1,36 +1,45 @@
-import Port1 from "../assets/images/E-commerce-store-template.png";
-import Port2 from "../assets/images/Facebook-clone.png";
-import Port3 from "../assets/images/Rivero.png";
-import Port4 from "../assets/images/linkedin-clone.png";
+import Port1 from "../assets/images/zyena.png";
+import Port2 from "../assets/images/minute-master.png";
+import Port3 from "../assets/images/relsify.png";
+import Port4 from "../assets/images/E-commerce-store-template.png";
 import Port5 from "../assets/images/Paks homes.png";
 import Port6 from "../assets/images/Admin Dashboard.png";
 
 export const ProjectData = [
   {
-    title: "E-Commerce store Template",
+    title: "Zyena",
     image: Port1,
-    url: "https://e-commerce-template-shop-it.onrender.com",
+    url: "https://zyena.vercel.app/",
     animationDelay: "w-1",
-    tools: "MERN Stack, Tailwind",
+    tools: "MERN Stack, Tailwindcss, MUI Library",
     id: 1,
   },
-
   {
-    title: "Rivero Trust Bank Plc",
-    image: Port3,
-    url: "https://rivero-trust-bank-plc.netlify.app",
+    title: "Minute Master",
+    image: Port2,
+    url: "https://minute-master.com/",
     animationDelay: "w-2",
-    tools: "NextJS, Typescript, SCSS",
+    tools: "NextJS, Typescript, GrapgQl, TailwindCss",
     id: 2,
   },
   {
-    title: "Paks Homes Real Estate",
-    image: Port5,
-    url: "https://pakshomes.netlify.app/",
-    animationDelay: "w-3",
-    tools: "REACT, Typescript, Firebase, SCSS",
+    title: "Relsify",
+    image: Port3,
+    url: "https://www.relsify.com",
+    animationDelay: "w-2",
+    tools: "REACT, Typescript",
     id: 3,
   },
+  {
+    title: "E-Commerce store Template",
+    image: Port4,
+    url: "https://e-commerce-template-shop-it.onrender.com",
+    animationDelay: "w-4",
+    tools: "MERN Stack, Tailwindcss, MUI Library",
+    id: 4,
+  },
+
+
   // {
   //   title: "Paks Everything Store",
   //   image: Port1,
